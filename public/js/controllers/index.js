797760ae66691a05207b103247ff3de64ff07fcb
Srinivasa Raju Indukuri <srinivasarajui@gmail.com>
2014-01-28 00:03:21 +0530

    default page set Stock

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('VTS.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
+angular.module('VTS.system').controller('IndexController', ['$scope', 'Global', '$location', function ($scope, Global, $location) {
     $scope.global = Global;
-  
+  	if(Global.authenticated){
+  		$location.path('stock');
+  	}
 }]);
